68fe4c594bc6a99e9ca3bb1bfbef4a207f88bb0b
lucasps16 <lupena@unal.edu.co>
2021-05-01 18:02:26 -0500

    Added cors support

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
+const cors = require( 'cors' )
  
 
  
 var app = express();
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema: schema.schema,
   rootValue: schema.root,
   graphiql: true,
 }));
+
+
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
