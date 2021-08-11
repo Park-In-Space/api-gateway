fe5d9d49026c3aa64ced201c2e92b748545b5e8c
Andres Ricardo Perez <riperezro@unal.edu.co>
2021-08-10 21:52:46 -0500

    GCP + LDAP

--- services/authenticationServices.js
+++ services/authenticationServices.js
@@ -1,7 +1,7 @@
 const { rejects } = require('assert');
 const http  = require('http');
 const { resolve } = require('path');
-const url = ` http://18.119.11.224:8080/users/`
+const url = ` http://10.88.14.140:8080/users/`
 const axios = require('axios')
 var plu = require('./plu-parkinglotuserService');
 var clu = require('./userServices');
